5f250af004a0607e964a6071676f7ff20352cd79
pandalapadu <59242614+pandalapadu@users.noreply.github.com>
2021-11-09 11:49:26 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 #variable "aws_access_key" {}
 #variable "aws_secret_key" {}
 variable "aws_region" {}
-variable "amis" {
-    description = "AMIs by region"
-    default = {
-        us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
-		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
-		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
-		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
-    }
-}
+# variable "amis" {
+#     description = "AMIs by region"
+#     default = {
+#         us-east-1 = "ami-97785bed" # ubuntu 14.04 LTS
+# 		us-east-2 = "ami-f63b1193" # ubuntu 14.04 LTS
+# 		us-west-1 = "ami-824c4ee2" # ubuntu 14.04 LTS
+# 		us-west-2 = "ami-f2d3638a" # ubuntu 14.04 LTS
+#     }
+# }
 variable "vpc_cidr" {}
 variable "vpc_name" {}
 variable "IGW_name" {}
@@ -28,11 +28,11 @@ variable "azs" {
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 variable "environment" { default = "dev" }
-variable "instance_type" {
-  default = {
-    dev = "t2.nano"
-    test = "t2.micro"
-    prod = "t2.medium"
-    }
-}
+# variable "instance_type" {
+#   default = {
+#     dev = "t2.nano"
+#     test = "t2.micro"
+#     prod = "t2.medium"
+#     }
+# }
 
